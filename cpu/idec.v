0d1cc2266bf73c3ec186a6ec29cd8817bbb8777a
dhy1996 <dhy1996@live.com.sg>
2020-01-21 11:47:37 -0800

    idec in progress

--- cpu/idec.v
+++ cpu/idec.v
@@ -1,3 +1,50 @@
-module idec () //instruction decoder
+module idec32  //instruction decoder
+    (iin, cpsrin,
+    alu_out, r1_out, r2_out,
+    cpsrs_out, reg_we, mem_we,
+    clk);
+    // instruction in, CPSR in, ALU out, R1 out, R2 out,
+    // should set CPSR out, register write enable, memory write enable,
+    // clock
+
+    input [31:0] iin;
+    input [3:0] cpsrin;
+    output reg [3:0] alu_out, r1_out, r2_out;
+    output reg cpsrs_out, reg_we, mem_we;
+    input clk;
+
+    wire [3:0] cond_intermediate, icond;
+    wire [2:0] opcode;
+    assign icond = iin[31:28];
+    assign cond_intermediate = cpsrin ~^ icond;
+    assign opcode = iin[27:25];
+
+    always_ff @(posedge clk) begin
+        // check condition, no-op (output 0) if dont match
+        if ( (icond & cond_intermediate) != 0) begin
+            alu_out <= 4'b0;
+            r1_out <= 4'b0;
+            r2_out <= 4'b0;
+            cpsrs_out <= 0;
+            reg_we <= 0;
+            mem_we <= 0;
+        end
+        else if ( opcode == 3'b00X ) begin // logical/arithmetic
+            alu_out <= iin[24:21];
+            cpsrs_out <= iin[20];
+            r1_out <= iin[19:16];
+            r2_out <= iin[15:12];
+        end
+        else if (opcode == 3'b01X) begin // load/store
+            reg_we <= iin[20]; // include or post increment?
+            mem_we <= ~iin[20];
+            r1_out <= iin[19:16];
+            r2_out <= iin[15:12];
+        end
+        else if (opcode == 3'b101) begin // branch
+            
+        end
+    end
+
 
 endmodule
---
+++ testcode/idec_test/idec_test.cpp
@@ -0,0 +1,72 @@
+#include <verilated.h>          // Defines common routines
+#include "Videc.h"
+
+#include "verilated_vcd_c.h"
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+
+Videc *uut;                     // Instantiation of module
+vluint64_t main_time = 0;       // Current simulation time
+
+double sc_time_stamp () {       // Called by $time in Verilog
+    return main_time;           // converts to double, to match
+    // what SystemC does
+}
+
+
+void vcdStep(Videc* uut, VerilatedVcdC* tfp, vluint64_t* main_time) {
+    uut->clk = !(uut->clk);
+    *main_time = *main_time + 1;
+    uut->eval();
+    if (tfp != NULL) {
+        tfp->dump (*main_time);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    // turn on trace or not?
+    bool vcdTrace = true;
+    VerilatedVcdC* tfp = NULL;
+
+    Verilated::commandArgs(argc, argv);   // Remember args
+    uut = new Videc;   // Create instance
+
+    uut->eval();
+    uut->eval();
+
+    if (vcdTrace)
+    {
+        Verilated::traceEverOn(true);
+
+        tfp = new VerilatedVcdC;
+        uut->trace(tfp, 99);
+
+        std::string vcdname = argv[0];
+        vcdname += ".vcd";
+        std::cout << vcdname << std::endl;
+        tfp->open(vcdname.c_str());
+    }
+    
+    // PUT INIT CODE HERE
+    uut->clk = 1;
+
+    // always sandwich between neg then pos
+    vcdStep(uut, tfp, &main_time);
+
+    uut->final();               // Done simulating
+
+    if (tfp != NULL)
+    {
+        tfp->close();
+        delete tfp;
+    }
+
+    delete uut;
+
+    return 0;
+}
+
