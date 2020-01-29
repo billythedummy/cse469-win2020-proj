4154192e89ef6fd58ff9af1bde58fa1989356218
dhy1996 <dhy1996@live.com.sg>
2020-01-29 11:21:47 -0800

    submission script

--- lint.sh
+++ lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJDIR=$(dirname "$0")
 if [ $1 == "all" ]; then
-    ls $PROJDIR/cpu | xargs verilator --lint-only -I$PROJDIR/cpu --top-module cpu
+    ls $PROJDIR/cpu | xargs verilator --lint-only -I$PROJDIR/cpu
 else
     verilator --lint-only -I$PROJDIR/cpu $1
 fi
