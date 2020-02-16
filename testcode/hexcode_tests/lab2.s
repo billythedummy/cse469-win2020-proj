88f3e9c4edb9759cca779d6b321658c64d70f7a4
dhy1996 <dhy1996@live.com.sg>
2020-02-16 11:22:55 -0800

    asm comments

--- testcode/hexcode_tests/lab2.s
+++ testcode/hexcode_tests/lab2.s
@@ -1,23 +1,23 @@
 .text
 .align 1
-.setup:
-    ADD r1, r1, #4
-.mainloop:
+.setup: @ 0x0
+    ADD r1, r1, #4 
+.mainloop:  @ 0x4
     LDR r0, [r1]
     BL  .inc
     STR r0, [r1]
     B   .mainloop
 
-.inc:
-    ADDS r0, r0, #1073741824
+.inc:       @ 0x14
+    ADDS r0, r0, #1073741824 
     BVS .overflowed
     MOV pc, lr
 
-.overflowed:
+.overflowed: @ 0x20
     BICS r0, r0, r0
     STR r0, [r1]
 
-.carryloop:
+.carryloop: @ 0x28
     LDR r2, [r1, #4]
     SUBS r2, r2, #2147483648
     STR r2, [r1, #4]
